842778faac2dada4c1b5f3e47bfb27a59fa6af01
ms-hacker404 <jtacd18@gmail.com>
2023-11-02 12:38:07 +1100

    home banner section changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -211,7 +211,7 @@ themeButton.addEventListener("click", () => {
 
 // Typing Animation using Typed JS
 var typed = new Typed(".type", {
-  strings: ["a Web", "an Android", "a Blockchain"],
+  strings: [" Web Developer", " Cybersecurity Enthusiast"],
   smartBackspace: true,
   startDelay: 1000,
   typeSpeed: 130,
